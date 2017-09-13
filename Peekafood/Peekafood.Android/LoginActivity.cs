cbe06102e8b08993ec05c567e3f728a1dd4df586
Taimoor Bin Khalid <taimoorkhalid95@gmail.com>
2017-09-13 22:46:36 +0500

    Basic facebook login functionality implemented

---
+++ Peekafood/Peekafood.Android/LoginActivity.cs
@@ -0,0 +1,69 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+
+using Android.Support.V7.App;
+using Xamarin.Facebook;
+using Xamarin.Facebook.Login.Widget;
+
+namespace Peekafood.Droid
+{
+    [Activity(Label = "LoginActivity", Icon = "@drawable/icon", Theme = "@style/Theme.MyTheme")]
+    public class LoginActivity : AppCompatActivity, IFacebookCallback
+    {
+        private ICallbackManager mCallBackManager;
+
+        public void OnCancel()
+        {
+            Toast.MakeText(this.ApplicationContext, "Cancelled", ToastLength.Short).Show();
+        }
+
+        public void OnError(FacebookException error)
+        {
+            Toast.MakeText(this.ApplicationContext, "Error", ToastLength.Short).Show();
+        }
+
+        public void OnSuccess(Java.Lang.Object result)
+        {
+            Intent i = new Intent(this, typeof(MainActivity));
+            StartActivity(i);
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.login);
+
+            if (IsLoggedIn())
+            {
+                Intent i = new Intent(this, typeof(MainActivity));
+                StartActivity(i);
+
+            }
+            else
+            {
+
+                LoginButton loginButton = FindViewById<LoginButton>(Resource.Id.login_button);
+
+                loginButton.SetReadPermissions("user_friends", "public_profile");
+                mCallBackManager = CallbackManagerFactory.Create();
+
+                loginButton.RegisterCallback(mCallBackManager, this);
+            }
+        }
+
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            mCallBackManager.OnActivityResult(requestCode, (int)resultCode, data);
+        }
+
+        public static bool IsLoggedIn()
+        {
+            AccessToken accessToken = AccessToken.CurrentAccessToken;
+            return accessToken != null;
+        }
+    }
+}
--- Peekafood/Peekafood.Android/MainActivity.cs
+++ Peekafood/Peekafood.Android/MainActivity.cs
@@ -4,19 +4,46 @@ using Peekafood.Droid.Fragments;
 
 using Android.Support.Design.Widget;
 using Android.Support.V7.App;
+using Android.Content.PM;
+using Java.Security;
+using System;
+using Xamarin.Facebook;
+using Xamarin.Facebook.Login.Widget;
+using Java.Lang;
+using Android.Content;
+using Android.Runtime;
+using Xamarin.Facebook.Login;
+using Android.Widget;
 
 namespace Peekafood.Droid
 {
     [Activity(Label = "@string/app_name", MainLauncher = true, LaunchMode = Android.Content.PM.LaunchMode.SingleTop, Icon = "@drawable/icon", Theme = "@style/Theme.MyTheme")]
     public class MainActivity : AppCompatActivity
     {
-
         BottomNavigationView bottomNavigation;
+
         protected override void OnCreate(Bundle bundle)
         {
 
             base.OnCreate(bundle);
+
+            //FacebookSdk.SdkInitialize(ApplicationContext);
+
             SetContentView(Resource.Layout.main);
+
+            CheckLoginStatus();
+
+            // Code to generate key hash
+            /*PackageInfo info = this.PackageManager.GetPackageInfo("com.peekafood", PackageInfoFlags.Signatures);
+
+            foreach (Android.Content.PM.Signature signature in info.Signatures) {
+                MessageDigest md = MessageDigest.GetInstance("SHA");
+                md.Update(signature.ToByteArray());
+
+                string keyhash = Convert.ToBase64String(md.Digest());
+                Console.WriteLine("KeyHash", keyhash);
+            }*/
+
             var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             if (toolbar != null)
             {
@@ -27,11 +54,45 @@ namespace Peekafood.Droid
             }
 
             bottomNavigation = FindViewById<BottomNavigationView>(Resource.Id.bottom_navigation);
-
-
             bottomNavigation.NavigationItemSelected += BottomNavigation_NavigationItemSelected;
 
             LoadFragment(Resource.Id.menu_home);
+
+            FindViewById<Button>(Resource.Id.logoutButton).Click += (o, e) =>
+            {
+                LoginManager.Instance.LogOut();
+
+                Intent i = new Intent(this, typeof(LoginActivity));
+                StartActivity(i);
+            };
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            CheckLoginStatus();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            CheckLoginStatus();
+        }
+
+        private void CheckLoginStatus()
+        {
+            if (!LoginActivity.IsLoggedIn())
+            {
+                Toast.MakeText(this.ApplicationContext, "Please login to continue", ToastLength.Short).Show();
+
+                Intent i = new Intent(this, typeof(LoginActivity));
+                StartActivity(i);
+            }
+            else
+            {
+                AccessToken accessToken = AccessToken.CurrentAccessToken;
+                //loadUserDetails(accessToken);
+            }
         }
 
         private void BottomNavigation_NavigationItemSelected(object sender, BottomNavigationView.NavigationItemSelectedEventArgs e)
